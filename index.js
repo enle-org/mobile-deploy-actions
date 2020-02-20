f619903e93d50422b5342b5191803b80e4df68c4
Ekpo David <dave170e71@gmail.com>
2020-02-20 09:16:58 +0100

    actions

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ try {
 
   const EXPO_USERNAME	 = core.getInput('expo_username');
   const EXPO_PASSWORD	 = core.getInput('expo_password');
+  const FIREBASE_TOKEN	 = core.getInput('firebase_token');
+  const FIREBASE_ANDROID_APP_ID	 = core.getInput('firebase_android_app_id');
 
-  exec.exec('curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install')
-    .then(() => exec.exec(`brew install wget`))
-    .then(() => exec.exec(`yarn`))
+  exec.exec(`yarn`)
     .then(() => exec.exec(`yarn global add expo-cli`))
+    .then(() => exec.exec(`curl -sL https://firebase.tools | bash`))
     .then(() => exec.exec(`yarn run expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}`))
     .then(() => exec.exec(`yarn build:android > output.txt`))
-    .then(() => exec.exec(`tail -n 2 ./output.txt | head -n 1 | cut -c47- | xargs wget`))
+    .then(() => exec.exec(`tail -n 2 ./output.txt | head -n 1 | cut -c47- | xargs wget -O build.apk`))
+    .then(() => exec.exec(`firebase appdistribution:distribute ./build.apk --app ${FIREBASE_ANDROID_APP_ID} --groups testers --token ${FIREBASE_TOKEN}`))
     .catch(e => core.setFailed(e));
     
 } catch (error) {
   core.setFailed(error.message);
 }
-
-// Successfully built standalone app: https://expo.io/artifacts/1b9a6ef8-db97-4274-8812-5d0865cb46fc
