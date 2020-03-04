2c52d458a1ec58c889e265e065a1ca7f442a9eef
Ekpo David <dave170e71@gmail.com>
2020-03-04 22:48:03 +0100

    actions

--- dist/index.js
+++ dist/index.js
@@ -19254,8 +19254,13 @@ try {
     .then(() => exec.exec(`yarn global add expo-cli`))
     .then(() => exec.exec(`yarn global add firebase-tools`))
     .then(() => exec.exec(`yarn run expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}`))
-    .then(() => exec.exec(`yarn build:android`))
-    .then(() => exec.exec(`tail -n 1 output.txt | head -n 1 | cut -c47- | xargs wget -O build.apk`))
+    // .then(() => exec.exec(`yarn build:android`))
+    .then(() => exec.exec(`tail -n 1 output.txt`))
+    .then(res => {
+      console.log("res", res)
+      exec.exec(`head -n 1`)
+    })
+    // .then(() => exec.exec(`tail -n 1 output.txt | head -n 1 | cut -c47- | xargs wget -O build.apk`))
     .then(() => exec.exec(`firebase appdistribution:distribute ./build.apk --app ${FIREBASE_ANDROID_APP_ID} --groups ${TEST_GROUP} --token ${FIREBASE_TOKEN}`))
     .catch(e => core.setFailed(e));
     
