6d2b3c69deeb088f508ebc33fa89a36d28c2fca2
Ekpo David <dave170e71@gmail.com>
2020-03-18 10:49:25 +0100

    actions

--- dist/index.js
+++ dist/index.js
@@ -1474,18 +1474,18 @@ try {
   const STAGING_API_URL = core.getInput('staging_api_url');
   const PROD_API_URL = core.getInput('prod_api_url');
 
-  const TEST_GROUP	 = core.getInput('test_group');
-  const EXPO_USERNAME	 = core.getInput('expo_username');
-  const EXPO_PASSWORD	 = core.getInput('expo_password');
-  const FIREBASE_TOKEN	 = core.getInput('firebase_token');
-  const FIREBASE_ANDROID_APP_ID	 = core.getInput('firebase_android_app_id');
+  const TEST_GROUP = core.getInput('test_group');
+  const EXPO_USERNAME = core.getInput('expo_username');
+  const EXPO_PASSWORD = core.getInput('expo_password');
+  const FIREBASE_TOKEN = core.getInput('firebase_token');
+  const FIREBASE_ANDROID_APP_ID = core.getInput('firebase_android_app_id');
 
   exec.exec(`yarn`)
     .then(() => exec.exec('yarn global add expo-cli'))
     .then(() => exec.exec('yarn global add firebase-tools'))
     .then(() => exec.exec(`yarn run expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}`))
     .then(() => exec.exec('chmod +x ./comands.sh'))
-    .then(() => exec.exec(`./comands.sh ${ENVIRONMENT} ${CLOUDINARY_CLOUDNAME} ${CLOUDINARY_URL} ${CLOUDINARY_API_KEY} ${CLOUDINARY_API_SECRET} ${CLOUDINARY_API_URL} ${CLOUDINARY_API_URL_STRING} ${TEST_USER_EMAIL} ${TEST_USER_PASSWORD} ${LOCAL_API_URL} ${DEV_API_URL} ${STAGING_API_URL} ${PROD_API_URL}`))
+    .then(() => exec.exec(`./commands.sh ${ENVIRONMENT} ${CLOUDINARY_CLOUDNAME} ${CLOUDINARY_URL} ${CLOUDINARY_API_KEY} ${CLOUDINARY_API_SECRET} ${CLOUDINARY_API_URL} ${CLOUDINARY_API_URL_STRING} ${TEST_USER_EMAIL} ${TEST_USER_PASSWORD} ${LOCAL_API_URL} ${DEV_API_URL} ${STAGING_API_URL} ${PROD_API_URL}`))
     // .then(() => exec.exec('yarn build:android'))
     // .then(() => exec.exec('yarn download-build'))
     .then(() => exec.exec(`firebase appdistribution:distribute ./build.apk --app ${FIREBASE_ANDROID_APP_ID} --groups ${TEST_GROUP} --token ${FIREBASE_TOKEN}`))
