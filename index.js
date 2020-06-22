0211ba777e72d96172b06ba52f66ad28757fcca1
Ekpo David <dave170e71@gmail.com>
2020-06-22 06:30:57 +0100

    actions

--- index.js
+++ index.js
@@ -38,16 +38,15 @@ try {
   const MATCH_GITHUB_USERNAME = core.getInput('match_github_username');
   const MATCH_GITHUB_PASSWORD = core.getInput('match_github_password');
 
-  // exec.exec('yarn global add expo-cli')
-  exec.exec('chmod +x ./deploy-scripts/create-files.sh')
-    // .then(() => exec.exec('yarn global add firebase-tools'))
-    // .then(() => exec.exec(`yarn run expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}`))
-    // .then(() => exec.exec('chmod +x ./deploy-scripts/create-files.sh'))
+  exec.exec('yarn global add expo-cli')
+    .then(() => exec.exec('yarn global add firebase-tools'))
+    .then(() => exec.exec(`yarn run expo login -u ${EXPO_USERNAME} -p ${EXPO_PASSWORD}`))
+    .then(() => exec.exec('chmod +x ./deploy-scripts/create-files.sh'))
     .then(() => exec.exec(`./deploy-scripts/create-files.sh ${ENVIRONMENT} ${CLOUDINARY_CLOUDNAME} ${CLOUDINARY_URL} ${CLOUDINARY_API_KEY} ${CLOUDINARY_API_SECRET} ${CLOUDINARY_API_URL} ${CLOUDINARY_API_URL_STRING} ${SENTRY_DSN} ${SENTRY_ORGANIZATION} ${SENTRY_PROJECT} ${SENTRY_AUTHTOKEN} ${TEST_USER_EMAIL} ${TEST_USER_PASSWORD} ${LOCAL_API_URL} ${DEV_API_URL} ${STAGING_API_URL} ${PROD_API_URL} ${MATCH_GITHUB_USERNAME} ${MATCH_GITHUB_PASSWORD}`))
-    // .then(() => exec.exec('yarn run expo p'))
-    // .then(() => exec.exec('chmod +x ./deploy-scripts/deploy.android.sh'))
-    // .then(() => exec.exec(`./deploy-scripts/deploy.android.sh ${ENVIRONMENT} ${CLOUDINARY_CLOUDNAME} ${CLOUDINARY_URL} ${CLOUDINARY_API_KEY} ${CLOUDINARY_API_SECRET} ${CLOUDINARY_API_URL} ${CLOUDINARY_API_URL_STRING} ${SENTRY_DSN} ${SENTRY_ORGANIZATION} ${SENTRY_PROJECT} ${SENTRY_AUTHTOKEN} ${TEST_USER_EMAIL} ${TEST_USER_PASSWORD} ${LOCAL_API_URL} ${DEV_API_URL} ${STAGING_API_URL} ${PROD_API_URL} ${ANDROID_KEYSTORE_PASSWORD}`))
-    // .then(() => exec.exec(`firebase appdistribution:distribute ./android/app/build/outputs/apk/release/app-release.apk --app ${FIREBASE_ANDROID_APP_ID} --groups ${TEST_GROUP} --token ${FIREBASE_TOKEN}`))
+    .then(() => exec.exec('yarn run expo p'))
+    .then(() => exec.exec('chmod +x ./deploy-scripts/deploy.android.sh'))
+    .then(() => exec.exec(`./deploy-scripts/deploy.android.sh ${ENVIRONMENT} ${CLOUDINARY_CLOUDNAME} ${CLOUDINARY_URL} ${CLOUDINARY_API_KEY} ${CLOUDINARY_API_SECRET} ${CLOUDINARY_API_URL} ${CLOUDINARY_API_URL_STRING} ${SENTRY_DSN} ${SENTRY_ORGANIZATION} ${SENTRY_PROJECT} ${SENTRY_AUTHTOKEN} ${TEST_USER_EMAIL} ${TEST_USER_PASSWORD} ${LOCAL_API_URL} ${DEV_API_URL} ${STAGING_API_URL} ${PROD_API_URL} ${ANDROID_KEYSTORE_PASSWORD}`))
+    .then(() => exec.exec(`firebase appdistribution:distribute ./android/app/build/outputs/apk/release/app-release.apk --app ${FIREBASE_ANDROID_APP_ID} --groups ${TEST_GROUP} --token ${FIREBASE_TOKEN}`))
     // .then(() => exec.exec('chmod +x ./deploy-scripts/deploy.ios.sh'))
     // .then(() => exec.exec(`./deploy-scripts/deploy.ios.sh ${ENVIRONMENT} ${CLOUDINARY_CLOUDNAME} ${CLOUDINARY_URL} ${CLOUDINARY_API_KEY} ${CLOUDINARY_API_SECRET} ${CLOUDINARY_API_URL} ${CLOUDINARY_API_URL_STRING} ${SENTRY_DSN} ${SENTRY_ORGANIZATION} ${SENTRY_PROJECT} ${SENTRY_AUTHTOKEN} ${TEST_USER_EMAIL} ${TEST_USER_PASSWORD} ${LOCAL_API_URL} ${DEV_API_URL} ${STAGING_API_URL} ${PROD_API_URL} ${APPLE_ID_EMAIL} ${APPLE_ID_PASSWORD}`))
     .catch(e => {
